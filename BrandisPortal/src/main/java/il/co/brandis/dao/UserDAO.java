f788b111c7336434f592c4b5e8ce1c341dab6783
orguz <orguz100@gmail.com>
2013-02-02 15:20:01 +0200

    test3

--- BrandisPortal/src/main/java/il/co/brandis/dao/UserDAO.java
+++ BrandisPortal/src/main/java/il/co/brandis/dao/UserDAO.java
@@ -40,7 +40,7 @@ public class UserDAO implements IUserDAO {
 		logger.debug("User has been deleted");
 
 	}
-//te
+//test
 	public User getUser(int uId) {
 		User user = (User) sessionFactory.getCurrentSession().get(User.class,
 				uId);
